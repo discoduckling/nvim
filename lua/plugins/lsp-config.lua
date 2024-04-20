7b094dcab2aa3ea041e616828d02ca3b9f7614ed
Mitalee Desai <mitaleedesai@honeycomb.io>
2024-04-20 01:09:40 -0700

    Add go support to nvim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,6 +4,11 @@ return {
 		config = function()
 			require("mason").setup()
 		end,
+		opts = {
+			ensure_installed = {
+				"gopls",
+			},
+		},
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
@@ -22,6 +27,21 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			lspconfig.tsserver.setup({})
+			local util = require("lspconfig.util")
+			lspconfig.gopls.setup({
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork", "gotmpl" },
+				root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+				settings = {
+					gopls = {
+						completeUnimported = true,
+						usePlaceholders = true,
+						analyses = {
+							unusedparams = true,
+						},
+					},
+				},
+			})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", ":vsplit | lua vim.lsp.buf.definition()<CR>", {})
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
